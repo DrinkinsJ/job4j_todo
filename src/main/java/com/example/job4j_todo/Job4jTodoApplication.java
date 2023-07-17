54c081a123f763f014ee4d20ff3993bfbaae9e3b
mypc <drinkinswo@gmail.com>
2023-07-17 04:08:09 +0300

    Initial commit

---
+++ src/main/java/com/example/job4j_todo/Job4jTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.job4j_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jTodoApplication.class, args);
+    }
+
+}
