eab2bb4d92d4003f32330ea226ac0bb366ec08c3
mypc <drinkinswo@gmail.com>
2023-07-18 16:21:18 +0300

    implementation SimpleTaskService

--- src/main/java/com/job4j/todo/services/SimpleTaskService.java
+++ src/main/java/com/job4j/todo/services/SimpleTaskService.java
@@ -1,4 +1,51 @@
 package com.job4j.todo.services;
 
+import com.job4j.todo.model.Task;
+import com.job4j.todo.repository.HbmTaskRepository;
+import com.job4j.todo.repository.TaskRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class SimpleTaskService implements TaskService {
+
+   private final TaskRepository taskRepository;
+    @Override
+    public Task create(Task task) {
+        return taskRepository.create(task);
+    }
+
+    @Override
+    public void update(Task task) {
+        taskRepository.update(task);
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return taskRepository.deleteById(id);
+    }
+
+    @Override
+    public Collection<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Collection<Task> findAllByDone(boolean isDone) {
+        return taskRepository.findAllByDone(isDone);
+    }
+
+    @Override
+    public Optional<Task> findById(int id) {
+        return taskRepository.findById(id);
+    }
+
+    @Override
+    public boolean setDone(int id, boolean isDone) {
+        return taskRepository.setDone(id, isDone);
+    }
 }
