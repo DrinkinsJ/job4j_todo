4e3279dca941755542158ecae9371f4bbafb5b35
mypc <drinkinswo@gmail.com>
2023-07-26 19:04:54 +0300

    6. Лямбды и шаблон Command [#49295 #432010] Сообщения fix#1

--- src/main/java/com/job4j/todo/repository/HbmUserRepository.java
+++ src/main/java/com/job4j/todo/repository/HbmUserRepository.java
@@ -2,6 +2,7 @@ package com.job4j.todo.repository;
 
 import com.job4j.todo.model.User;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
@@ -9,14 +10,21 @@ import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
+@Slf4j
 public class HbmUserRepository implements UserRepository {
 
     private final CrudRepository crudRepository;
 
     @Override
     public Optional<User> save(User user) {
-        crudRepository.run(session -> session.persist(user));
-        return Optional.ofNullable(user);
+        Optional<User> optionalUser = Optional.empty();
+        try {
+            crudRepository.run(session -> session.persist(user));
+            optionalUser = Optional.of(user);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return optionalUser;
     }
 
     @Override
